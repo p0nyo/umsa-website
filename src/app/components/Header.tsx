4fa89f993445550349ec962b14d48a10777f9144
Treyson Tsen <treysontsen@treyson-mba3.local>
2024-12-24 17:53:41 +1300

    feat: added metadata

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,15 +3,15 @@ export default function Header() {
     return(
         <div className="">
             <div className="grid grid-cols-[1fr,auto,1fr] items-center text-white text-center">
-                <div className="flex justify-end text-4xl font-semibold space-x-4 mx-4">
+                <div className="flex justify-end text-3xl font-semibold space-x-4 mx-4">
                     <p className="">about</p>
                     <p className="">events</p>
                 </div>
                 <div className="justify-self-center">
-                    <h1 className="text-9xl font-bold leading-none">umsa</h1>
-                    <h1 className="text-4xl font-light">new zealand</h1>
+                    <h1 className="text-8xl font-bold leading-none">umsa</h1>
+                    <h1 className="text-2xl font-light">new zealand</h1>
                 </div>
-                <div className="flex justify-start text-4xl font-semibold space-x-4 mx-4">
+                <div className="flex justify-start text-3xl font-semibold space-x-4 mx-4">
                     <p>our team</p>
                     <p>signup</p>
                 </div>
