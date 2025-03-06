ec5bb489149cff2112dbc600a66368474891c091
treyson tsen <78147374+p0nyo@users.noreply.github.com>
2025-03-07 00:46:52 +1300

    edit: faq answer

--- src/app/components/pages/Faq.tsx
+++ src/app/components/pages/Faq.tsx
@@ -3,7 +3,7 @@ import Bounded from "../Bounded"
 const FaqItems = [
     { question: "How do I become a member of UMSA?", answer: "Shoot an e-mail to our helpful communication officers at comm.umsanz@gmail.com and we will be more the merrier to help you get things sorted out. You can also sign up online! Simply click on this link, and follow the step-by-step guide, and you're good to go! During the first week of every new semester, catch our booth at the University of Auckland's club expo."},
     { question: "I'm not Malaysian, can I still join?", answer: "The more the merrier! You do not have to be a Malaysian to join the UMSA family. We happily welcome people from all cultures and backgrounds."},
-    { question: "Does it cost to join UMSA?", answer: "The price to join UMSA is $7 - for both new and returning members."},
+    { question: "Does it cost to join UMSA?", answer: "The price to join UMSA is $8 - for both new and returning members."},
     { question: "I'm not a UOA student, can I still join?", answer: "Absolutely! In fact, invite any other friends outside of the University of Auckland to join the family as well!"},
     { question: "How do I find out or keep myself updated on UMSA's events?", answer: "Make sure to follow our social media platforms! Keep yourself updated by checking UMSA's website, Facebook and Instagram constantly. We will be updating you with up-coming events, opportunities and what is going on with the community."}
 ]
