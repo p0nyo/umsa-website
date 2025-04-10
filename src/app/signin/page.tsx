678386196856bc991b6e2178ff909f635c3700c5
treyson tsen <78147374+p0nyo@users.noreply.github.com>
2025-04-10 17:10:08 +1200

    add: sign in page frontend

---
+++ src/app/signin/page.tsx
@@ -0,0 +1,18 @@
+export default function signIn() {
+    return(
+        <div className="relative flex flex-col min-h-screen justify-center items-center bg-starImg bg-cover">
+            <div className="absolute inset-0 opacity-65 bg-blueOverlay"></div>
+            <div className="flex-col justify-center items-center z-40 space-y-20">
+                <img className="pointer-events-none sm:h-auto transparent-y-gradient" draggable="false" src="/umsa-globe1.svg"/>
+                <div>
+                    <a href="/signin">
+                        <div className="border-4 border-white bg-white rounded-md flex justify-center items-center scale-hover">
+                            <p className="text-umsaBlue text-2xl">sign in with</p>
+                            <img src="/google.svg"></img>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    )
+}
